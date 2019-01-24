b023e29ee9e8f64543e86de4024896e4bb163046
Amandine V <amavelamala@gmail.com>
2019-01-24 15:54:03 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,1 +1,8 @@
 // add solution here
+function theBeatlesPlay(musicians, instruments) {
+  var whoPlaysWhat = [];
+  for (let i = 0; i < musicians.length; i++){
+    whoPlaysWhat.push(`${musicians[i]} plays ${instruments[i]}`)
+  }
+  return whoPlaysWhat;
+}
